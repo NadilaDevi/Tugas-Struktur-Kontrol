d964037f7a558ca82fa6e78be0e463f127566d0b
NadilaDevi <72852926+NadilaDevi@users.noreply.github.com>
2020-10-14 15:41:25 +0700

    Add files via upload

---
+++ Tugas Struktur Kontrol/src/semangatoke/PraKe3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semangatoke;
+
+
+public class PraKe3 {
+    public static void main (String [] args){
+        String nama;
+        int gaji;
+        
+        nama = "Nadila Devi Tsabita";
+        gaji = 10000000;
+        
+        // if else switch
+        if(gaji > 9000000){
+             System.out.println("Mendapat bonus gaji");
+        }
+        else{
+             System.out.println("Mendapat gaji tetap");
+        }
+    }
+    
+}
